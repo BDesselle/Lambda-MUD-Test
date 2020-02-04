3ee158dcbac2f43d8660fd36ebf454b3fdc68cdf
[Brandon] <desselle.brandon2@gmail.com>
2020-02-04 16:44:03 -0600

    Initial Files

---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Textbox from './Textbox'
+import './Sidebar.css'
+const Sidebar = (props) => {
+
+  return (
+    <div className='sidebar'>
+      <Textbox/>
+      {/* will need to adjust styling */}
+      <div onClick={props.changeCount} className="map_button">MAP</div>
+    </div>
+  )
+}
+
+export default Sidebar
